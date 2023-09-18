0f8bdb012ab2117c462cfa33843b4438e8634f3f
JNeaderland <144148081+JNeaderland@users.noreply.github.com>
2023-09-18 12:17:02 -0400

    Add files via upload

---
+++ lab_2-3.py
@@ -0,0 +1,14 @@
+#Author: Jacob Neaderland
+a = 2
+print (type(a))
+print (a)
+a = float(a)
+print (type(a))
+print (a)
+a = str(a)
+print (type(a))
+print (a)
+a = bool(a)
+a == 2
+print (type(a))
+print (a)
